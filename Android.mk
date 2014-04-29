3d6257a3ef2955c19c13291c0300179fabe50c7d
Renaud de Chivre <renaud.de.chivre@intel.com>
2014-04-29 17:14:01 +0200

    Update Android.mk for new plugin location
    BZ: 190767
    
    - Add license header.
    - Update relative paths to match new plugin location.
    - Update stlport import way
    - Reorder few statements
    
    Change-Id: I68aaebe0514624c65c9b6b39d1742b852ba0b311
    Signed-off-by: Renaud de Chivre <renaud.de.chivre@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,39 +1,48 @@
+# INTEL CONFIDENTIAL
+#
+# Copyright 2014 Intel Corporation All Rights Reserved.
+# The source code contained or described herein and all documents
+# related to the source code ("Material") are owned by Intel
+# Corporation or its suppliers or licensors. Title to the Material
+# remains with Intel Corporation or its suppliers and licensors. The
+# Material contains trade secrets and proprietary and confidential
+# information of Intel or its suppliers and licensors. The Material is
+# protected by worldwide copyright and trade secret laws and treaty
+# provisions. No part of the Material may be used, copied, reproduced,
+# modified, published, uploaded, posted, transmitted, distributed, or
+# disclosed in any way without Intel's prior express written permission.
+#
+# No license under any patent, copyright, trade secret or other
+# intellectual property right is granted to or conferred upon you by
+# disclosure or delivery of the Materials, either expressly, by
+# implication, inducement, estoppel or otherwise. Any license under
+# such intellectual property rights must be express and approved by
+# Intel in writing.
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/parameter-framework-plugins/Fs
 
 LOCAL_SRC_FILES := \
     FSSubsystem.cpp \
     FSSubsystemBuilder.cpp \
     FSSubsystemObject.cpp
 
+LOCAL_CFLAGS := \
+    -Wall \
+    -Werror \
+    -Wextra \
+    -Wno-unused-parameter    # Needed to workaround STL bug
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/../../core/parameter \
+    $(LOCAL_PATH)/../../core/xmlserializer \
+
+LOCAL_SHARED_LIBRARIES := libparameter
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/parameter-framework-plugins/Fs
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libfs-subsystem
 
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_CFLAGS += \
-        -Wall \
-        -Werror \
-        -Wextra \
-        -Wno-unused-parameter
-
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/../parameter \
-    $(LOCAL_PATH)/../xmlserializer/
-
-LOCAL_C_INCLUDES += \
-        external/stlport/stlport/ \
-        bionic/ \
-        vendor/intel/hardware/include
-
-LOCAL_C_INCLUDES +=
-
-LOCAL_SHARED_LIBRARIES := libstlport libparameter
-
-LOCAL_LDLIBS +=
-LOCAL_LDFLAGS +=
-
+include external/stlport/libstlport.mk
 include $(BUILD_SHARED_LIBRARY)
-
-
